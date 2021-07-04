da7f4b456015a7b0ac8270edaa651028c6c217f5
cagdastombul <cagdastombul@gmail.com>
2021-07-04 23:18:33 +0300

    tests for moving rover backward are written.

--- src/test/java/com/marsrover/rover/RoverTest.java
+++ src/test/java/com/marsrover/rover/RoverTest.java
@@ -21,23 +21,27 @@ public class RoverTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideDirectionAndExpectedCoordinateForMoveForward")
-    void moveWithCommandsShouldMoveForwardInRoverDirectionWhenCommandIsF(Direction direction, Coordinate expectedCoordinate) {
+    @MethodSource("provideDirectionAndExpectedCoordinateForMoveForwardAndBackward")
+    void moveWithCommandsShouldMoveForwardInRoverDirectionWhenCommandIsF(Command command, Direction direction, Coordinate expectedCoordinate) {
 
         rover.setDirection(direction);
-        rover.moveWithCommands(Command.FORWARD.toString());
+        rover.moveWithCommands(command.toString());
 
         assertEquals(direction, rover.getDirection());
         assertEquals(expectedCoordinate.getX(), rover.getCoordinate().getX());
         assertEquals(expectedCoordinate.getY(), rover.getCoordinate().getY());
     }
 
-    private static Stream<Arguments> provideDirectionAndExpectedCoordinateForMoveForward(){
+    private static Stream<Arguments> provideDirectionAndExpectedCoordinateForMoveForwardAndBackward(){
 
         return Stream.of(
-                Arguments.of(Direction.NORTH, new Coordinate(rover.getCoordinate().getX(), rover.getCoordinate().getY() + 1)),
-                Arguments.of(Direction.EAST, new Coordinate(rover.getCoordinate().getX() + 1, rover.getCoordinate().getY())),
-                Arguments.of(Direction.SOUTH, new Coordinate(rover.getCoordinate().getX(), rover.getCoordinate().getY() - 1)),
-                Arguments.of(Direction.WEST, new Coordinate(rover.getCoordinate().getX() - 1, rover.getCoordinate().getY())));
+                Arguments.of(Command.FORWARD, Direction.NORTH, new Coordinate(rover.getCoordinate().getX(), rover.getCoordinate().getY() + 1)),
+                Arguments.of(Command.FORWARD, Direction.EAST, new Coordinate(rover.getCoordinate().getX() + 1, rover.getCoordinate().getY())),
+                Arguments.of(Command.FORWARD, Direction.SOUTH, new Coordinate(rover.getCoordinate().getX(), rover.getCoordinate().getY() - 1)),
+                Arguments.of(Command.FORWARD, Direction.WEST, new Coordinate(rover.getCoordinate().getX() - 1, rover.getCoordinate().getY())),
+                Arguments.of(Command.BACKWARD, Direction.NORTH, new Coordinate(rover.getCoordinate().getX(), rover.getCoordinate().getY() - 1)),
+                Arguments.of(Command.BACKWARD, Direction.EAST, new Coordinate(rover.getCoordinate().getX() - 1, rover.getCoordinate().getY())),
+                Arguments.of(Command.BACKWARD, Direction.SOUTH, new Coordinate(rover.getCoordinate().getX(), rover.getCoordinate().getY() + 1)),
+                Arguments.of(Command.BACKWARD, Direction.WEST, new Coordinate(rover.getCoordinate().getX() + 1, rover.getCoordinate().getY())));
     }
 }
